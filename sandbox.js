c7eef51eea909756e54788bfbeee687ff39d2551
haiip <Nikita.magnusson@live.se>
2019-10-17 20:44:16 +0200

    formating

--- sandbox.js
+++ sandbox.js
@@ -1,90 +1,78 @@
 function animatedForm() {
 
-  const arrows = document.querySelectorAll(".fa-arrow-down");
+	const arrows = document.querySelectorAll(".fa-arrow-down");
 
-  arrows.forEach(arrow => {
-    arrow.addEventListener("click", () => {
-      const input = arrow.previousElementSibling;
-      const parent = arrow.parentElement;
-      const nextForm = parent.nextElementSibling;
+	arrows.forEach(arrow => {
 
-      //check for validation
+		arrow.addEventListener("click", () => {
+			const input = arrow.previousElementSibling;
+			const parent = arrow.parentElement;
+			const nextForm = parent.nextElementSibling;
 
-      if (input.type === "text" && validateUser(input)) {
+			//check for validation
+			if (input.type === "text" && validateUser(input)) {
+				nextSlide(parent, nextForm);
+			}
+			else if (input.type === "email" && validateEmail(input)) {
+				nextSlide(parent, nextForm);
+			}
+			else if (input.type === "password" && validateUser(input)) {
+				nextSlide(parent, nextForm);
+			}
+			else {
+				parent.style.animation = "shake 0.5s ease"
+			}
 
-        nextSlide(parent, nextForm);
-      } else if (input.type === "email" && validateEmail(input)) {
-        nextSlide(parent, nextForm);
-      } else if (input.type === "password" && validateUser(input)) {
-        nextSlide(parent, nextForm);
-      } else {
-        parent.style.animation = "shake 0.5s ease"
-      }
-      parent.addEventListener("animationend", () => {
+			parent.addEventListener("animationend", () => {
+				parent.style.animation = "";
+			});
 
-        parent.style.animation = "";
+		});
 
-      })
-    });
-  });
+	});
 
 }
 
 function validateUser(user) {
-  if (user.value.length < 6) {
-    console.log("not enough characters");
-    error("rgb(189,87,87");
-  } else {
-    error("orange");
-    return true;
-  }
+	if(user.value.length < 6) {
+		console.log("not enough characters");
+		error("rgb(189,87,87");
+	}
+	else {
+		error("orange");
+		return true;
+	}
 }
 
 function validateEmail(email) {
 
-  const validation = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-  if (validation.test(email.value)) {
-    error("orange");
-    return true;
-  } else {
-    error("rgb(189,87,87");
-  }
+	const validation = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+	if (validation.test(email.value)) {
+		error("orange");
+		return true;
+	}
+	else {
+		error("rgb(189,87,87");
+	}
+
 }
 
 function nextSlide(parent, nextForm) {
-  parent.classList.add("innactive");
-  parent.classList.remove("active");
-  nextForm.classList.add("active");
-
+	parent.classList.add("innactive");
+	parent.classList.remove("active");
+	nextForm.classList.add("active");
 }
 
-
-
-
 function error(color) {
-  document.body.style.backgroundColor = color;
+	document.body.style.backgroundColor = color;
 }
 
 animatedForm();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 var media = document.getElementById("myvideo");
 const playPromise = media.play();
-if (playPromise !== null) {
-  playPromise.catch(() => { media.play(); })
-}
 
+if(playPromise !== null) {
+	playPromise.catch(() => { media.play(); })
+}
